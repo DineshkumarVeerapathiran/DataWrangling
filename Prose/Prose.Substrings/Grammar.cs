f6d53e9fc3f9444d46dd0420ac66d21854d7c310
DineshkumarVeerapathiran <dineshkumar.veerapathiran@syncfusion.com>
2023-07-03 13:04:56 +0530

    Changes

---
+++ Prose/Prose.Substrings/Grammar.cs
@@ -0,0 +1,20 @@
+﻿using AngularData.Prose.Prose.Substrings;
+using System.IO;
+using System.Reflection;
+
+namespace AngularData.Prose.Prose.Substrings
+{
+    public static class GrammarText
+    {
+        public static string Get()
+        {
+            //AngularData.Prose.ProseSample.Substrings.Semantics.g
+            var assembly = typeof(GrammarText).GetTypeInfo().Assembly;
+            using (var stream = assembly.GetManifestResourceStream("AngularData.Prose.Prose.Substrings.Prose.Substrings.grammar"))
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
