c2e858bbb6b15bc46bf4e4d43a053fa6c9c41c03
DineshkumarVeerapathiran <dineshkumar.veerapathiran@syncfusion.com>
2023-07-05 10:06:26 +0530

    Changes

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SpreadsheetComponent } from '@syncfusion/ej2-angular-spreadsheet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: "<ejs-spreadsheet (beforeOpen)='beforeOpen($event)' openUrl='https://services.syncfusion.com/angular/production/api/spreadsheet/open' allowOpen='true'> </ejs-spreadsheet>",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  @ViewChild('spreadsheet')
+  public spreadsheetObj: SpreadsheetComponent | undefined;
+
+  public onCreate() {
+    //this.spreadsheetObj?.beforeCellFormat
+  }
 }
